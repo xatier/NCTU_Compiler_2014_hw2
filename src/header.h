74d68ff502aa9f7f2b8963f47d85c8ae3c4e0f51
xatier <xatierlike@gmail.com>
2014-04-07 19:14:06 +0800

    style fix for symbol table

--- src/header.h
+++ src/header.h
@@ -1,11 +1,11 @@
-struct symtab{
-	char lexeme[256];
-	struct symtab *front;
-	struct symtab *back;
-	int line;
-	int counter;
+struct symtab {
+    char lexeme[256];
+    struct symtab *front;
+    struct symtab *back;
+    int line;
+    int counter;
 };
 
 typedef struct symtab symtab;
-symtab * lookup(char *name);
+symtab *lookup(char *name);
 void insert(char *name);
