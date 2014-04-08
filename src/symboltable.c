81c5717ec3688305a3d37bc5b3fe764d4e1b4f34
xatier <xatierlike@gmail.com>
2014-04-08 21:45:30 +0800

    sort the symbol table before print

--- src/symboltable.c
+++ src/symboltable.c
@@ -68,19 +68,59 @@ void insertID (char *name) {
 }
 
 void printSym (symtab *ptr) {
-    printf("Name = %s \n", ptr->lexeme);
-    printf("References = %d \n", ptr->counter);
+    printf("%-20s %d \n", ptr->lexeme, ptr->counter);
+}
+
+int cmpEntry (const void *a, const void *b) {
+    return strcmp(((const struct symtab *)a)->lexeme,
+                  ((const struct symtab *)b)->lexeme);
 }
 
 void printSymTab (void) {
     int i;
-    printf("----- Symbol Table ---------\n");
+    int entry_count;
+
+    // a copy of hash table for sorted print
+    symtab *hash_table_p;
+
+    // get entries count
     for (i = 0; i < TABLE_SIZE; i++) {
-        symtab *symptr;
-        symptr = hash_table[i];
+        symtab *symptr = hash_table[i];
         while (symptr != NULL) {
-            printf("====>  index = %d \n", i);
-            printSym(symptr);
+            symptr = symptr->front;
+            entry_count++;
+        }
+    }
+
+    hash_table_p = (struct symtab *)malloc(sizeof(struct symtab) * entry_count);
+
+    int cur;
+    for (i = 0, cur = 0; i < TABLE_SIZE; i++) {
+        symtab *symptr = hash_table[i];
+        while (symptr != NULL) {
+            memcpy(&hash_table_p[cur], symptr, sizeof(struct symtab));
+            symptr = symptr->front;
+            cur++;
+        }
+    }
+
+    // sort before printing
+    qsort(hash_table_p, entry_count, sizeof(struct symtab), cmpEntry);
+
+    printf("----- Symbol Table ---------\n");
+    for (i = 0; i < entry_count; i++) {
+        printSym(&hash_table_p[i]);
+    }
+
+
+    // be a good programmer
+    free(hash_table_p);
+
+    for (i = 0; i < TABLE_SIZE; i++) {
+        symtab *symptr = hash_table[i];
+        while (symptr != NULL) {
+            symtab *this = symptr;
+            free(this);
             symptr = symptr->front;
         }
     }
